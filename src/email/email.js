e8fb4d48c42021a2033a65bf658756d15e755570
nadaGamal25 <gnada1221@gmail.com>
2024-12-30 13:28:46 +0200

    update email

--- src/email/email.js
+++ src/email/email.js
@@ -15,7 +15,21 @@ export const sendEmail=(email,otpCode)=>{
         from: 'Forira',
         to: email,
         subject: 'Password Reset Code',
-        text: `Your OTP code is: ${otpCode}. It will expire in 10 minutes.`
+        // text: `Your OTP code is: ${otpCode}. It will expire in 10 minutes.`,
+        html: `
+        <div style="
+            text-align: center;
+            background-color: #fff;
+            box-shadow: 5px 5px 10px #8ca0aa;
+            border-radius: 5px;
+            padding: 20px;
+            font-family: Arial, sans-serif;
+        ">
+            <p style="font-size: 18px; color: #333;">الكود الخاص بك هو</p>
+            <p style="font-size: 24px; color: #000; font-weight: bold;">${otpCode}</p>
+            <p style="font-size: 14px; color: #555;">.سوف تنتهى صلاحيته خلال 10 دقائق</p>
+        </div>
+    `
       };
     
       transporter.sendMail(mailOptions, (error, info) => {
