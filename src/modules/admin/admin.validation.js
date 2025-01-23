c7fb3de0dac401100c4ad3e01233b75baf42c4b7
nadaGamal25 <gnada1221@gmail.com>
2025-01-23 09:59:52 +0200

    up

--- src/modules/admin/admin.validation.js
+++ src/modules/admin/admin.validation.js
@@ -24,78 +24,78 @@ const highlightUserVal=Joi.object({
 })
 const updateOrderVal = Joi.object({
     id:Joi.string().hex().length(24).required(),
-    clientAddress: Joi.string()
+    clientAddress: Joi.string().allow('',null)
         .min(0)
         .messages({
             'string.base': 'يجب أن يكون العنوان صحيحاً.',
             'string.min': 'يجب أن يكون العنوان 1 أحرف على الأقل.',
         }),
-    recieverAddress: Joi.string()
+    recieverAddress: Joi.string().allow('',null)
         .min(0)
         
         .messages({
             'string.base': 'يجب أن يكون عنوان المستلم صحيحاً.',
             'string.min': 'يجب أن يكون عنوان المستلم حرفاً واحداً على الأقل.',
         }),
-    clientName: Joi.string()
+    clientName: Joi.string().allow('',null)
         
         .messages({
             'string.base': 'يجب أن يكون اسم العميل صحيحاً.',
         }),
-    recieverName: Joi.string()
+    recieverName: Joi.string().allow('',null)
         .messages({
             'string.base': 'يجب أن يكون اسم المستلم صحيحاً.',
         }),
-    clientPhone: Joi.string()
+    clientPhone: Joi.string().allow('',null)
         
         .messages({
             'string.base': 'يجب أن يكون رقم العميل صحيحاً.',
         }),
-    recieverPhone: Joi.string()
+    recieverPhone: Joi.string().allow('',null)
         .messages({
             'string.base': 'يجب أن يكون رقم المستلم صحيحاً.',
         }),
-    goDate: Joi.date()
+    goDate: Joi.date().allow('',null)
         
         .messages({
             'date.base': 'يجب أن يكون التاريخ صالحاً.',
         }),
-    nums: Joi.number()
+    nums: Joi.number().allow('',null)
         
         .messages({
             'number.base': 'يجب أن يكون العدد رقماً صحيحاً.',
         }),
-    price: Joi.number()
+    price: Joi.number().allow('',null)
         
         .messages({
             'number.base': 'يجب أن يكون السعر رقماً صحيحاً.',
         }),
-    type: Joi.string()
+    type: Joi.string().allow('',null)
         
         .messages({
             'string.base': 'يجب أن يكون النوع صحيحاً.',
         }),
-    goTime: Joi.string()
+    goTime: Joi.string().allow('',null)
         .min(1)
         
         .messages({
             'string.base': 'يجب أن يكون وقت الانطلاق صحيحاً.',
             'string.min': 'يجب أن يكون وقت الانطلاق حرفاً واحداً على الأقل.',
         }),
-    waitingTime: Joi.string()
+    waitingTime: Joi.string().allow('',null)
         .messages({
             'string.base': 'يجب أن يكون وقت الانتظار صحيحاً.',
         }),
-    notes: Joi.string()
+    notes: Joi.string().allow('',null)
         .messages({
             'string.base': 'يجب أن تكون الملاحظات نصاً صحيحاً.',
         }),
-    isTips: Joi.boolean()
+    isTips: Joi.boolean().allow('',null)
         
         .messages({
             'boolean.base': 'يجب أن تكون النصيحة قيمة صحيحة.',
         }),
-    clientId: Joi.string()
+    clientId: Joi.string().allow('',null)
         .hex()
         .length(24)
         
@@ -118,21 +118,21 @@ const updateOrderVal = Joi.object({
             'string.hex': 'يجب أن يكون موقع المستلم بتنسيق صحيح (سداسي عشري).',
             'string.length': 'يجب أن يكون موقع المستلم 24 حرفاً.',
         }),
-    orderImgs: Joi.array()
+    orderImgs: Joi.array().allow('',null)
         .items()
         .min(0)
         .messages({
             'array.base': 'يجب أن يكون الصور مصفوفة.',
         }),
-    deliveryType:Joi.string().messages({
+    deliveryType:Joi.string().allow('',null).messages({
         'string.base': 'يجب أن يكون نوع التوصيل قيمة صحيحة.',
 
     })  ,
-    payType:Joi.string().messages({
+    payType:Joi.string().allow('',null).messages({
         'string.base': 'يجب أن يكون نوع التوصيل قيمة صحيحة.',
 
     }),
-    shopping: Joi.array().items(
+    shopping: Joi.array().allow('',null).items(
         Joi.object({
             store: Joi.string()
                 .messages({
